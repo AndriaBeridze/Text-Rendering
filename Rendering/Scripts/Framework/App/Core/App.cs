38ed49336a74ccdd189619b55a09008e50560973
AndriaBeridze <andria24b@gmail.com>
2024-11-29 16:34:50 -0500

    Set up Console

---
+++ Rendering/Scripts/Framework/App/Core/App.cs
@@ -0,0 +1,7 @@
+namespace Rendering.App;
+
+class App {
+    public App() {
+        Console.WriteLine("Text Rendering");
+    }
+}
---
+++ Rendering/Scripts/Framework/App/Core/Program.cs
@@ -0,0 +1,7 @@
+﻿namespace Rendering.App;
+
+class Program {
+    static void Main() {
+        App app = new App();
+    }
+}
